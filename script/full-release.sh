a6b60521461ae49a24b7cc58d1179ddb979dd04f
XBigTK13X <xbigtk13x@gmail.com>
2026-08-16 06:41:14 -0500

    Ensure failing builds/scripts stop a deploymnet

--- script/full-release.sh
+++ script/full-release.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+set -e
+
 source script/variables.sh
 
 export NODE_ENV="production"
@@ -11,30 +13,24 @@ if [ ! -z $1 ]; then
 fi
 
 if [ "$MODE" == "server" ] || [ "$MODE" == "all" ]; then
+    echo "=-=- Building the container image -=-="
+    script/docker-build.sh push
 
-echo "=-=- Building the container image -=-="
-set -e
-script/docker-build.sh push
-set +e
-
-echo "=-=- Running the latest version container on beast -=-="
-ssh access@beast.9914.us "bash -c \"cd /mnt/docker; ./on/snowgroove.sh\""
-
+    echo "=-=- Running the latest version container on beast -=-="
+    ssh access@beast.9914.us "bash -c \"cd /mnt/docker; ./on/snowgroove.sh\""
 fi
 
 if [ "$MODE" == "client" ] || [ "$MODE" == "all" ]; then
+    echo "=-=- Build the apks -=-="
+    script/prod-generate-apks.sh
 
-echo "=-=- Build the apks -=-="
-set -e
-script/prod-generate-apks.sh
-set +e
+    echo "=-=- Push the apks up to the file server -=-="
+    ~/script/push-apks.py snowgroove
 
-echo "=-=- Push the apks up to the file server -=-="
-~/script/push-apks.py snowgroove
-
-echo "=-=- Deploy the apks to all devices -=-="
-~/script/remote-adb.py All deploy_snowgroove
-
+    echo "=-=- Deploy the apks to all devices -=-="
+    ~/script/remote-adb.py All deploy_snowgroove
 fi
 
 unset NODE_ENV
+
+set +e
