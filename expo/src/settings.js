fa872aa25d568d6744e062b8707d3b25ad8f6eac
XBigTK13X <xbigtk13x@gmail.com>
2026-08-23 19:14:50 -0500

    Snowui with the rotation fix

--- expo/src/settings.js
+++ expo/src/settings.js
@@ -4,8 +4,8 @@ class Config {
         this.stormWebApiUrl = 'http://192.168.104.113:10063' // Laptop
         this.beastWebApiUrl = 'http://beast.9914.us:10063' // Prod
 
-        this.clientVersion = "1.1.14"
-        this.clientBuildDate = "August 21, 2026"
+        this.clientVersion = "1.1.15"
+        this.clientBuildDate = "August 23, 2026"
         this.clientDevBuildNumber = 1
 
         this.debounceMilliseconds = 700
