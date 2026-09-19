d22e8b33cb591eb57cab99c5d32f689d53a9e380
XBigTK13X <xbigtk13x@gmail.com>
2026-09-19 06:02:13 -0500

    version bump

--- web-server/settings.py
+++ web-server/settings.py
@@ -4,8 +4,8 @@ import sys
 
 class Config:
     def __init__(self):
-        self.server_version = "1.2.7"
-        self.server_build_date = "September 01, 2026"
+        self.server_version = "1.3.0"
+        self.server_build_date = "September 19, 2026"
         self.server_build_dev_number = 1
         self.app_data_dir = '.snowgroove/'
         self.display_config = None
