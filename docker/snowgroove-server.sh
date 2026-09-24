cbdd571e9ab9d409cec4f493b744aa297ae36097
XBigTK13X <xbigtk13x@gmail.com>
2026-09-23 22:23:12 -0500

    Fix the docker migration script

--- docker/snowgroove-server.sh
+++ docker/snowgroove-server.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
 echo "Updating the database with the latest schema"
-/app/script/db-migrate.sh docker
+uv run alembic -c /app/docker/alembic.ini upgrade head
+rabbitmqctl add_user snowgroove snowgroove
+rabbitmqctl set_user_tags snowgroove administrator
+rabbitmqctl set_permissions -p / snowgroove ".*" ".*" ".*"
 
 echo "Launching the web api"
 uv run uvicorn --fd 0 bin.server:app --log-config /app/docker/uvicorn-log.yaml
